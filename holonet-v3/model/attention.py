8be93dcc2e0da2c653cc8e2bfc6d7cdde40b652b
almonzeir <monzeer2002@gmail.com>
2026-02-25 23:45:34 +0800

    Implement LocalSniperAttention using PyTorch MultiheadAttention with causal mask

--- holonet-v3/model/attention.py
+++ holonet-v3/model/attention.py
@@ -1,7 +1,17 @@
-class LocalSniperAttention:
-    def __init__(self):
-        pass
+import torch
+from torch import nn
 
-    def forward(self, x):
-        # Implementation of forward method
-        return x
+class LocalSniperAttention(nn.Module):
+    def __init__(self, embed_dim, num_heads):
+        super(LocalSniperAttention, self).__init__()
+        self.multihead_attn = nn.MultiheadAttention(embed_dim, num_heads)
+
+    def forward(self, query, key, value, attn_mask=None):
+        attn_output, attn_weights = self.multihead_attn(query, key, value, attn_mask=attn_mask)
+        return attn_output, attn_weights
+
+
+def causal_mask(size):
+    # Generate a causal mask
+    mask = torch.triu(torch.ones(size, size), diagonal=1).bool()
+    return mask
